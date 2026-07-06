79273a977a0767ed348d86b709a66973126e6ec8
hudong <hello@hudong.cn>
2026-07-06 19:20:06 +0800

    Initial commit

---
+++ configs/webpack.common.conf.js
@@ -0,0 +1,269 @@
+const Vue = require('vue'); // 确保已在顶部引入
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+//
+const path = require('path');
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const config = require('./config');
+const helper = require('./helper');
+const glob = require('glob');
+const vueLoaderConfig = require('./vue-loader.conf');
+const vueWebTemp = helper.rootNode(config.templateDir);
+const hasPluginInstalled = fs.existsSync(helper.rootNode(config.pluginFilePath));
+const isWin = /^win/.test(process.platform);
+const weexEntry = {
+  'index': helper.root('entry.js')
+}
+
+const getEntryFileContent = (source, routerpath) => {
+  let dependence = `import Vue from 'vue'\n`;
+  dependence += `import weex from 'weex-vue-render'\n`;
+  let relativePluginPath = helper.rootNode(config.pluginFilePath);
+  let entryContents = fs.readFileSync(source).toString();
+  let contents = '';
+  entryContents = dependence + entryContents;
+  entryContents = entryContents.replace(/\/\* weex initialized/, match => `weex.init(Vue)\n${match}`);
+  if (isWin) {
+    relativePluginPath = relativePluginPath.replace(/\\/g, '\\\\');
+  }
+  if (hasPluginInstalled) {
+    contents += `\n// If detact plugins/plugin.js is exist, import and the plugin.js\n`;
+    contents += `import plugins from '${relativePluginPath}';\n`;
+    contents += `plugins.forEach(function (plugin) {\n\tweex.install(plugin)\n});\n\n`;
+    entryContents = entryContents.replace(/\.\/router/, routerpath);
+    entryContents = entryContents.replace(/weex\.init/, match => `${contents}${match}`);
+  }
+  return entryContents;
+}
+
+const getRouterFileContent = (source) => {
+  const dependence = `import Vue from 'vue'\n`;
+  let routerContents = fs.readFileSync(source).toString();
+  routerContents = dependence + routerContents;
+  return routerContents;
+}
+
+const getEntryFile = () => {
+  const entryFile = path.join(vueWebTemp, config.entryFilePath)
+  const routerFile = path.join(vueWebTemp, config.routerFilePath)
+  fs.outputFileSync(entryFile, getEntryFileContent(helper.root(config.entryFilePath), routerFile));
+  fs.outputFileSync(routerFile, getRouterFileContent(helper.root(config.routerFilePath)));
+  return {
+    index: entryFile
+  }
+}
+
+// The entry file for web needs to add some library. such as vue, weex-vue-render
+// 1. src/entry.js
+// import Vue from 'vue';
+// import weex from 'weex-vue-render';
+// weex.init(Vue);
+// 2. src/router/index.js
+// import Vue from 'vue'
+const webEntry = getEntryFile();
+
+
+
+/**
+ * Plugins for webpack configuration.
+ */
+const plugins = [
+  new VueLoaderPlugin(), // 👈 添加这一行
+  /**
+   * Plugin: webpack.DefinePlugin
+   * Description: The DefinePlugin allows you to create global constants which can be configured at compile time.
+   *
+   * See: https://webpack.js.org/plugins/define-plugin/
+   */
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': config.dev.env
+    }
+  }),
+  /*
+   * Plugin: BannerPlugin
+   * Description: Adds utils banner to the top of each generated chunk.
+   * See: https://webpack.js.org/plugins/banner-plugin/
+   */
+  new webpack.BannerPlugin({
+    banner: '// { "framework": "Vue"} \n',
+    raw: true,
+    exclude: 'Vue'
+  })
+];
+
+// Config for compile jsbundle for web.
+const webConfig = {
+  mode: 'development',// 显式设置 mode 为 development
+  //
+  entry: Object.assign(webEntry, {
+    'vendor': [path.resolve('node_modules/phantom-limb/index.js')]
+  }),
+  output: {
+    path: helper.rootNode('./dist'),
+    filename: '[name].web.js'
+  },
+  /**
+   * Options affecting the resolving of modules.
+   * See http://webpack.github.io/docs/configuration.html#resolve
+   */
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': helper.resolve('src')
+    }
+  },
+  /*
+   * Options affecting the resolving of modules.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#module
+   */
+  module: {
+    // webpack 2.0
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader'
+        }],
+        exclude: config.excludeModuleReg
+      },
+      {
+        test: /\.vue(\?[^?]+)?$/,
+        use: [{
+          loader: 'vue-loader',
+          options: {
+            ...vueLoaderConfig({ useVue: true, usePostCSS: false, vue: Vue }),
+            // options: Object.assign(vueLoaderConfig({useVue: true, usePostCSS: false}), {
+            // options: vueLoaderConfig({useVue: true, usePostCSS: false}, {
+            // vue: Vue, // 显式注入 Vue 实例
+            compilerOptions: {
+              modules: [
+                {
+                  postTransformNode: el => {
+                    require('weex-vue-precompiler')()(el)
+                  }
+                }
+              ]
+            },
+            optimizeSSR: false,
+            postcss: [
+              // to convert weex exclusive styles.
+              require('postcss-plugin-weex')(),
+              require('autoprefixer')({
+                browsers: ['> 0.1%', 'ios >= 8', 'not ie < 12']
+              }),
+              require('postcss-plugin-px2rem')({
+                // base on 750px standard.
+                rootValue: 75,
+                // to leave 1px alone.
+                minPixelValue: 1.01
+              })
+            ],
+            compilerModules: [
+              {
+                postTransformNode: el => {
+                  // to convert vnode for weex components.
+                  require('weex-vue-precompiler')()(el)
+                }
+              }
+            ]
+          }
+        }],
+        exclude: config.excludeModuleReg
+      }
+    ]
+  },
+  /*
+   * Add additional plugins to the compiler.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#plugins
+   */
+  plugins: plugins
+};
+// Config for compile jsbundle for native.
+const weexConfig = {
+  mode: 'development', // 显式设置 mode 为 development
+  entry: weexEntry,
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js'
+  },
+  /**
+   * Options affecting the resolving of modules.
+   * See http://webpack.github.io/docs/configuration.html#resolve
+   */
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': helper.resolve('src')
+    }
+  },
+  /*
+   * Options affecting the resolving of modules.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#module
+   */
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader'
+        }],
+        exclude: config.excludeModuleReg
+      },
+      // {
+      //   test: /\.vue(\?[^?]+)?$/,
+      //   use: [{
+      //     loader: 'weex-loader',
+      //     options: vueLoaderConfig({ useVue: false })
+      //   }],
+      //   exclude: config.excludeModuleReg
+      // },
+      {
+        test: /\.vue(\?[^?]+)?$/,
+        use: [{
+          loader: 'vue-loader',
+          options: vueLoaderConfig({ useVue: true, vue: Vue })
+        }]
+      },
+      {
+        resourceQuery: /^\?vue/,
+        use: [
+          'vue-loader/lib/template-compiler/index?transformToRequire=%7B%22img%22%3A%20%5B%22src%22%5D%7D'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  /*
+   * Add additional plugins to the compiler.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#plugins
+   */
+  plugins: plugins,
+  /*
+  * Include polyfills or mocks for various node stuff
+  * Description: Node configuration
+  *
+  * See: https://webpack.github.io/docs/configuration.html#node
+  */
+  node: config.nodeConfiguration
+};
+
+module.exports = [webConfig, weexConfig];
