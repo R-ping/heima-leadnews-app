2528cd9f85957c0252d12d67533de33438d1f9d1
hudong <hello@hudong.cn>
2026-07-24 20:20:28 +0800

    refactor: 提取LevelScoreConstants常量类
    从LevelServiceImpl中提取3组积分常量到独立类
    
    ACTION_SCORE_MAP/DAILY_ACTION_LIMIT/POWER_ACTION_LIMIT/DAILY_SCORE_LIMIT

--- heima-leadnews/heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/LevelServiceImpl.java
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/LevelServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
+import static com.heima.article.constants.LevelScoreConstants.*;
+
 @Slf4j
 @Service
 public class LevelServiceImpl implements LevelService {
@@ -32,49 +34,6 @@ public class LevelServiceImpl implements LevelService {
 
     @Autowired
     private ApUserPermissionMapper userPermissionMapper;
-
-    private static final int DAILY_SCORE_LIMIT = 200;
-
-    private static final Map<String, Integer> ACTION_SCORE_MAP = new HashMap<>();
-    static {
-        ACTION_SCORE_MAP.put("daily_login", 10);
-        ACTION_SCORE_MAP.put("article_read", 2);
-        ACTION_SCORE_MAP.put("comment_article", 2);
-        ACTION_SCORE_MAP.put("comment_pin", 2);
-        ACTION_SCORE_MAP.put("like_article", 1);
-        ACTION_SCORE_MAP.put("like_pin", 1);
-        ACTION_SCORE_MAP.put("share", 3);
-        ACTION_SCORE_MAP.put("follow_user", 4);
-        ACTION_SCORE_MAP.put("publish_article", 8);
-        ACTION_SCORE_MAP.put("publish_pins", 2);
-        ACTION_SCORE_MAP.put("daily_checkin", 2);
-        ACTION_SCORE_MAP.put("upload_avatar", 1);
-        ACTION_SCORE_MAP.put("collect_article", 1);
-        ACTION_SCORE_MAP.put("browse_article", 0);
-        ACTION_SCORE_MAP.put("browse_course", 0);
-    }
-
-    private static final Map<String, Integer> DAILY_ACTION_LIMIT = new HashMap<>();
-    static {
-        DAILY_ACTION_LIMIT.put("publish_article", 2);
-        DAILY_ACTION_LIMIT.put("publish_pins", 2);
-        DAILY_ACTION_LIMIT.put("comment_article", 5);
-        DAILY_ACTION_LIMIT.put("comment_pin", 5);
-        DAILY_ACTION_LIMIT.put("like_article", 10);
-        DAILY_ACTION_LIMIT.put("like_pin", 10);
-        DAILY_ACTION_LIMIT.put("follow_user", 2);
-        DAILY_ACTION_LIMIT.put("daily_checkin", 1);
-        DAILY_ACTION_LIMIT.put("upload_avatar", 1);
-        DAILY_ACTION_LIMIT.put("collect_article", 2);
-        DAILY_ACTION_LIMIT.put("browse_article", 10);
-        DAILY_ACTION_LIMIT.put("browse_course", 10);
-        DAILY_ACTION_LIMIT.put("daily_login", 2);
-    }
-
-    private static final Map<String, Integer> POWER_ACTION_LIMIT = new HashMap<>();
-    static {
-        POWER_ACTION_LIMIT.put("publish_article", 2);
-    }
 
     @Override
     public ApUserLevel getUserLevel(Long userId) {
